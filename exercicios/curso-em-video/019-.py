072dc9c7b1f1e3ccc8a0358c7d246a9269cc1e3e
Lorena <dandaratempo@gmail.com>
2025-08-29 22:45:13 -0300

    to com sono (exercicios 19 e 20 do curso em video)

--- exercicios/curso-em-video/019-.py
+++ exercicios/curso-em-video/019-.py
@@ -1,2 +1,6 @@
 # Um professor quer sortear um dos seus alunos para apagar o quadro.
 # Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
+
+n1 = int(input("Escreva um numero: "))
+n2 = int(input("Escreva outro numero: "))
+print("Ola a soma dos numero é: ", n1 + n2)
